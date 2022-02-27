7d38b7965b5402a4da00574b4725bcd5fb3dce62
Lil-El <yxd99324@qq.com>
2022-02-27 16:31:01 +0800

    scss file export variables

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { createApp } from "vue";
+import ElementPlus from 'element-plus';
 import router from "./router";
+import store from './store'
 import App from "./App.vue";
+import '@/style/index.scss'
 
-const app = createApp(App);
-
-app.use(router);
-app.mount("#app");
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(ElementPlus)
+  .mount('#app')
